b692e06bd22f82ba2b76d4bb8848230d235f337a
ugorox <ugorox@f0dc1310-df5f-11dd-b58c-9b3bf6bc8b46>
2009-08-27 23:26:16 +0000

    Goals can be created, linked to elements, showed up

--- trunk/br.puc.molic.diagram/src/br/puc/molic/diagram/actions/BindGoalAction.java
+++ trunk/br.puc.molic.diagram/src/br/puc/molic/diagram/actions/BindGoalAction.java
@@ -10,6 +10,8 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.draw2d.Shape;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.edit.command.SetCommand;
 import org.eclipse.emf.edit.provider.IItemPropertySource;
 import org.eclipse.emf.edit.ui.celleditor.FeatureEditorDialog;
 import org.eclipse.emf.workspace.AbstractEMFOperation;
@@ -20,6 +22,7 @@ import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeNodeEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.figures.DiagramColorConstants;
 import org.eclipse.gmf.runtime.diagram.ui.internal.properties.Properties;
+import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditor;
 import org.eclipse.gmf.runtime.diagram.ui.requests.ChangePropertyValueRequest;
 import org.eclipse.gmf.runtime.draw2d.ui.figures.FigureUtilities;
 import org.eclipse.gmf.runtime.draw2d.ui.figures.PolylineConnectionEx;
@@ -39,57 +42,62 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.views.IViewDescriptor;
 
 import br.puc.molic.Connection;
 import br.puc.molic.Diagram;
 import br.puc.molic.Element;
+import br.puc.molic.MolicPackage;
+import br.puc.molic.diagram.edit.parts.BRTUtteranceEditPart;
 import br.puc.molic.diagram.part.MolicDiagramEditor;
+import br.puc.molic.diagram.views.GoalsView;
 
 
 
 public class BindGoalAction implements IObjectActionDelegate {
 
-    boolean DEBUG = false;
-    IWorkbenchPart selectedPart;
-    /**
-     * @generated
-     */
-    private IStructuredSelection mySelectedElements;
+	boolean DEBUG = false;
+	IWorkbenchPart selectedPart;
+	/**
+	 * @generated
+	 */
+	private IStructuredSelection mySelectedElements;
 
-    /**
-     * @generated
-     */
-    private Shell myShell;
+	/**
+	 * @generated
+	 */
+	private Shell myShell;
 
-    /**
-     * @generated
-     */
-    public void setActivePart(IAction action, IWorkbenchPart targetPart) {
-        myShell = targetPart.getSite().getShell();
-        selectedPart = targetPart;
-        //System.out.println(selectedPart);
-    }
+	/**
+	 * @generated
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		myShell = targetPart.getSite().getShell();
+		selectedPart = targetPart;
+		//System.out.println(selectedPart);
+	}
 
-    /**
-     * @generated
-     */
-    public void run(IAction action) {
-       //System.out.println("binding "+mySelectedElements);
+	/**
+	 * @generated
+	 */
+	public void run(IAction action) {
+		//System.out.println("binding "+mySelectedElements);
 
-        /*
+		/*
         Shell dialog = new Shell (myShell.getDisplay(), SWT.DIALOG_TRIM);
-    	
+
     	Table table = new Table(dialog, SWT.CHECK | SWT.BORDER );
     	table.setHeaderVisible(false);
     	table.setLinesVisible(false);
-    		  
+
     	CheckboxTableViewer viewer = new CheckboxTableViewer(table);
-    	    
+
     	Button okButton = new Button (dialog, SWT.PUSH);
     	okButton.setText ("&OK");
     	Button cancelButton = new Button (dialog, SWT.PUSH);
     	cancelButton.setText ("&Cancel");
-    	
+
     	FormLayout form = new FormLayout ();
     	form.marginWidth = form.marginHeight = 8;
     	dialog.setLayout (form);
@@ -100,127 +108,123 @@ public class BindGoalAction implements IObjectActionDelegate {
     	cancelData.left = new FormAttachment (okButton, 8);
     	cancelData.top = new FormAttachment (okButton, 0, SWT.TOP);
     	cancelButton.setLayoutData (cancelData);
-    	
+
     	dialog.setDefaultButton (okButton);
     	dialog.pack ();
     	dialog.open ();
-    	
+
     	while (!myShell.getDisplay().isDisposed ()) {
     		if (!myShell.getDisplay().readAndDispatch ()) myShell.getDisplay().sleep ();
     	}
     	//display.dispose ();
-    	*/
-        /*
-    	FeatureEditorDialog dialog = new FeatureEditorDialog(
-				myShell, getLabelProvider(),
-				getObject(), feature.getEType(),
-				(List) ((IItemPropertySource) itemPropertyDescriptor.getPropertyValue(object)).getEditableValue(object), getDisplayName(),
-				choiceOfValues);
-		dialog.open();
-		*/
-    	
-    	MolicDiagramEditor editor = (MolicDiagramEditor)selectedPart;
-     
-     	Diagram d = (Diagram)editor.getDiagramEditPart().getDiagramView().getElement();
-    	
-    	
-    	Iterator it = mySelectedElements.iterator();
-       while(it.hasNext()) {
-          
-           
-           Object o = it.next();
-           if(o instanceof ShapeNodeEditPart) {
-               ShapeNodeEditPart n = (ShapeNodeEditPart) o;
-               Element e = (Element)((NodeImpl)n.getModel()).basicGetElement();
-               if(DEBUG)System.out.println(e.getID());
-               
-               //changeColor(n, DiagramColorConstants.red);
+		 */
 
-               //n.getFigure().setForegroundColor(DiagramColorConstants.red);
-              
-               Shape s = (Shape)n.getFigure().getChildren().get(0);
-            	  
-               s.setLineWidth(3);            	  
-               s.setForegroundColor(DiagramColorConstants.red);
-            
-             
-               
-               
-           }else {
-               ConnectionNodeEditPart n = (ConnectionNodeEditPart) o;  
+		try{
+			MolicDiagramEditor editor = (MolicDiagramEditor)selectedPart;
 
-               
-               Connection e = (Connection)((EdgeImpl)n.getModel()).basicGetElement();
-               if(DEBUG)System.out.println(e.getID());
-               
-               PolylineConnectionEx s = (PolylineConnectionEx)n.getFigure();
-               s.setLineWidth(3);               
-               s.setForegroundColor(DiagramColorConstants.red);
-               
-              // changeColor(n, DiagramColorConstants.red);
-           }
-           
-       }
-    }
+			Diagram d = (Diagram)editor.getDiagramEditPart().getDiagramView().getElement();
+			GoalsView view = (GoalsView) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView("br.puc.molic.diagram.views.GoalsView");   
+
+			//editor.getSite().getPage().g
+			Iterator it = mySelectedElements.iterator();
+			while(it.hasNext()) {
+
+				Object o = it.next();
+				if(DEBUG) System.out.println(o);
+				if(o instanceof ShapeNodeEditPart) {
+					ShapeNodeEditPart n = (ShapeNodeEditPart) o;
+					Element e = (Element)((NodeImpl)n.getModel()).basicGetElement();
 
 
-    protected Object openDialogBox(){
-    /*  InputDialog dialog = new MultiLineInputDialog(myShell,EMFEditUIPlugin.INSTANCE.getString
+
+					EAttribute feature = MolicPackage.eINSTANCE.getElement_Goals();
+					DiagramEditor activeEditor = (DiagramEditor) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+					activeEditor.getEditingDomain().getCommandStack().execute(SetCommand.create(activeEditor.getEditingDomain(), e, feature , view.getCheckedGoals()));
+
+
+					if(DEBUG)System.out.println(e.getID());
+
+
+				}else{
+					ConnectionNodeEditPart n = (ConnectionNodeEditPart) o;  
+
+
+					Connection e = (Connection)((EdgeImpl)n.getModel()).basicGetElement();
+					EAttribute feature = MolicPackage.eINSTANCE.getConnection_Goals();
+					DiagramEditor activeEditor = (DiagramEditor) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+					activeEditor.getEditingDomain().getCommandStack().execute(SetCommand.create(activeEditor.getEditingDomain(), e, feature , view.getCheckedGoals()));
+
+
+
+					if(DEBUG)System.out.println(e.getID());
+
+				}
+
+			}
+			view.highlightChecked();
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+
+
+	protected Object openDialogBox(){
+		/*  InputDialog dialog = new MultiLineInputDialog(myShell,EMFEditUIPlugin.INSTANCE.getString
            ("_UI_FeatureEditorDialog_title", new Object [] { getDisplayName(), getEditLabelProvider().getText(object) }),
          EMFEditUIPlugin.INSTANCE.getString("_UI_MultiLineInputDialog_message"),
          valueHandler.toString(getValue()),
          valueHandler);
       return dialog.open() == Window.OK ? valueHandler.toValue(dialog.getValue()) : null;
-      */
-    	return null;
-    }
-    
-    private void changeColor(IGraphicalEditPart part, Color c){
-    	 try {
-             ChangePropertyValueRequest req = new ChangePropertyValueRequest( 
-          	        StringStatics.BLANK,Properties.ID_LINECOLOR,FigureUtilities.colorToInteger(c));
+		 */
+		return null;
+	}
 
-             final Command cmd = (Command) part.getCommand(req);
-             
-             AbstractEMFOperation operation = new AbstractEMFOperation(((IGraphicalEditPart) part).getEditingDomain(),
-             	StringStatics.BLANK, null) {
-             	protected IStatus doExecute(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
-             		cmd.execute();
-             		return Status.OK_STATUS;
-             	}
-             };
+	private void changeColor(IGraphicalEditPart part, Color c){
+		try {
+			ChangePropertyValueRequest req = new ChangePropertyValueRequest( 
+					StringStatics.BLANK,Properties.ID_LINECOLOR,FigureUtilities.colorToInteger(c));
 
-             
-             	operation.execute(new NullProgressMonitor(), null);
-             } catch (Exception ex) {
-              ex.printStackTrace();
-             }
-    }
-    
-    
-    /**
-     * @generated
-     */
-    public void selectionChanged(IAction action, ISelection selection) {
-       
-        mySelectedElements = null;
-        if (selection instanceof IStructuredSelection) {
-            IStructuredSelection structuredSelection = (IStructuredSelection) selection;
-            mySelectedElements = structuredSelection;
-        }
-        
-        //System.out.println(selection);
-      //  if(!selection.isEmpty())
-        	//action.setEnabled(isEnabled());
-         
-         
-    }
+			final Command cmd = (Command) part.getCommand(req);
 
-    /**
-     * @generated
-     */
-    private boolean isEnabled() {
-        return mySelectedElements != null;
-    }
+			AbstractEMFOperation operation = new AbstractEMFOperation(((IGraphicalEditPart) part).getEditingDomain(),
+					StringStatics.BLANK, null) {
+				protected IStatus doExecute(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
+					cmd.execute();
+					return Status.OK_STATUS;
+				}
+			};
+
+
+			operation.execute(new NullProgressMonitor(), null);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+
+	/**
+	 * @generated
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+
+		mySelectedElements = null;
+		if (selection instanceof IStructuredSelection) {
+			IStructuredSelection structuredSelection = (IStructuredSelection) selection;
+			mySelectedElements = structuredSelection;
+		}
+
+		//System.out.println(selection);
+		//  if(!selection.isEmpty())
+		//action.setEnabled(isEnabled());
+
+
+	}
+
+	/**
+	 * @generated
+	 */
+	private boolean isEnabled() {
+		return mySelectedElements != null;
+	}
 
 }
